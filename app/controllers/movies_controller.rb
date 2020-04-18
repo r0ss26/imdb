bead50f56324549ac8aea7783b6f8fea071670b3
Ross Baker <rossbaker32@gmail.com>
2020-04-18 15:34:41 +1000

    added a form for creating movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,20 @@
+class MoviesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    puts params
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/movies", to: "movies#index"
+  get "/movies", to: "movies#index", as: "movies"
+  get "/movies/new", to: "movies#new"
   get "/movies/:id", to: "movies#show"
 
   post "/movies", to: "movies#create"
