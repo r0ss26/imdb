90afe2a46783946dfbf556bcbe98172770fe3c50
Ross Baker <rossbaker32@gmail.com>
2020-04-18 15:20:22 +1000

    added routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/movies", to: "movies#index"
+  get "/movies/:id", to: "movies#show"
+
+  post "/movies", to: "movies#create"
+
+  put "/movies/:id", to: "movies#update"
+  patch "/movies/:id", to: "movies#update"
+
+  delete "/movies/:id", to: "movies#destroy"
 end
