e47353547eb7867f3016f73a45256ebd515d042b
Ross Baker <rossbaker32@gmail.com>
2020-04-18 17:55:58 +1000

    added navigation buttons and form for editing movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,41 @@
 class MoviesController < ApplicationController
   def index
+    @movies = session[:movies]
+    # puts @movies
   end
 
   def show
+    @movie = session[:movies][params[:id].to_i - 1]
   end
 
   def create
-    puts params
+    if session[:movies] == nil
+      session[:movies] = []
+    end
+    
+    session[:movies].push(params[:movie])
+
+    puts "hello: #{session}"
+
+    redirect_to movie_path(session[:movies].length - 1)
   end
 
   def update
+    puts "session #{session[:movies]}"
+    @movie = session[:movies][params[:id].to_i - 1]
+    puts "movie #{@movie}"
+    @movie["title"] = params[:movie]["title"]
+    @movie["rating"] = params[:movie]["rating"]
+
+    redirect_to movie_path(params[:id])
   end
 
   def destroy
   end
 
   def new
+  end
+
+  def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#index", as: "movies"
-  get "/movies/new", to: "movies#new"
-  get "/movies/:id", to: "movies#show"
+  get "/movie/new", to: "movies#new", as: "new_movie"
+  get "/movie/:id", to: "movies#show", as: "movie"
+  get "/movie/:id/edit", to: "movies#edit", as: "edit"
 
   post "/movies", to: "movies#create"
 
-  put "/movies/:id", to: "movies#update"
-  patch "/movies/:id", to: "movies#update"
+  put "/movie/:id", to: "movies#update"
+  patch "/movie/:id", to: "movies#update"
 
-  delete "/movies/:id", to: "movies#destroy"
+  delete "/movie/:id", to: "movies#destroy"
 end
